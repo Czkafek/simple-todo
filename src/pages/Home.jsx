00fb3117a93a5e1513b261561de1bbc582f1cb4e
Czkafek <czkafek@gmail.com>
2025-02-01 12:28:25 +0100

    u can check tasks, they get line-through and also local storage remembers everything

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,52 @@
-import React, {  useState } from "react";
+import React, {  useState, useEffect, useRef } from "react";
 import styles from './Home.module.css'
 
 import AddInterface from "../components/AddInterface/AddInterface";
 
 function Home() {
 
-    const [tasks, setTasks] = useState(["Add your tasks"]);
+    const [tasks, setTasks] = useState([{text: "Add your tasks", checked: false}]);
+    const isFirstRender = useRef(true);
 
     const addTask = (task) => {
-        setTasks(t => [...t, task]);
+        setTasks(t => [...t, { text: task, checked: false }]);
     }
+
+    const handleCheck = (index) => {
+        setTasks((prevTasks) =>
+            prevTasks.map((task, i) =>
+              i === index ? { ...task, checked: !task.checked } : task
+            )
+          );
+    };
+
+    useEffect(() => {
+        const data = localStorage.getItem("tasks");
+        console.log(data);
+        if(data) {
+            setTasks(JSON.parse(data));
+        }
+        console.log(data);
+    }, []);
+
+    useEffect(() => {
+        if (isFirstRender.current) {
+            isFirstRender.current = false;
+            return;
+        }
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }, [tasks]);
 
     return(
         <>
             <h1>To-Do List</h1>
 
             <div className={styles.tasks}>
-                {tasks.map((value, index) => 
+                {tasks.map((task, index) => 
                     <div key={index} className={styles.task}>
-                        <input type="checkbox" />
+                        <input type="checkbox" onChange={() => handleCheck(index)} />
                         <span className={styles.radio}/>
-                        <p>{value}</p>
+                        <p className={task.checked ? styles.strikethrough : ''}>{task.text}</p>
                     </div> 
                 )}
             </div>
