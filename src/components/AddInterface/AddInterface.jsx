f0bf0a676dfea68915fd99d57be7be0db6674296
Czkafek <czkafek@gmail.com>
2025-02-04 21:46:09 +0100

    todo app is officialy done

--- src/components/AddInterface/AddInterface.jsx
+++ src/components/AddInterface/AddInterface.jsx
@@ -1,23 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from './AddInterface.module.css'
 
 function AddInterface({ addTask }) {
 
     const [inputValue, setInputValue] = useState("");
+    const [change, setChange] = useState(window.innerHeight > 900 ? true : false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
         if (inputValue.trim() === "") return;
         addTask(inputValue);
         setInputValue("");
     }
-    
+
+    const handleInputChange = (e) => setInputValue(e.target.value);
+
+    useEffect(() => {
+        const updateWindowDimensions = () => {
+            setChange(window.innerHeight > 900);
+        };
+
+        window.addEventListener("resize", updateWindowDimensions);
+
+        return () => window.removeEventListener("resize", updateWindowDimensions);
+
+    }, []);
+
 
     return(
         <>
-            <form onSubmit={handleSubmit} className={styles.form}>
-                <input id="taskInput" value={inputValue} onChange={(e) => setInputValue(e.target.value)} type="text" placeholder="Read, make dinner..."/>
+            <form onSubmit={handleSubmit} className={change ? styles.limitHeight : ''}>
+                <input id="taskInput" value={inputValue} onChange={handleInputChange} type="text" placeholder="Read, make dinner..."/>
                 <button type="submit">Add task</button>
             </form>
         </>
